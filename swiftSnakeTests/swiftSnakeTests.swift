db5803712864a313d3349fec154e27cbe151f753
gliadev <gliatem@gmail.com>
2025-03-18 00:04:27 +0100

    Initial Commit

---
+++ swiftSnakeTests/swiftSnakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftSnakeTests.swift
+//  swiftSnakeTests
+//
+//  Created by Adolfo on 18/3/25.
+//
+
+import Testing
+@testable import swiftSnake
+
+struct swiftSnakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
