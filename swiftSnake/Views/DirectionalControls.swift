47f83106fe83e98d6a335b488dc1eafb5d1316b2
gliadev <gliatem@gmail.com>
2026-03-30 00:33:15 +0200

    Algo así: feat: migrate to @Observable

---
+++ swiftSnake/Views/DirectionalControls.swift
@@ -0,0 +1,18 @@
+//
+//  DirectionalControls.swift
+//  swiftSnake
+//
+//  Created by Adolfo on 29/03/2026.
+//
+
+import SwiftUI
+
+struct DirectionalControls: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DirectionalControls()
+}
